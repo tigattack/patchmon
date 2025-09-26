60fa59880334f26cae452075573d19fd90dcac60
tigattack <10629864+tigattack@users.noreply.github.com>
2025-09-26 19:59:56 +0100

    fix(frontend): solve missing imports

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
@@ -16,11 +17,11 @@ const queryClient = new QueryClient({
 });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
+	<StrictMode>
 		<BrowserRouter>
 			<QueryClientProvider client={queryClient}>
 				<App />
 			</QueryClientProvider>
 		</BrowserRouter>
-	</React.StrictMode>,
+	</StrictMode>,
 );
